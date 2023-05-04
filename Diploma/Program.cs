59c393d941f90210434f3e0a475ebf4e504d4dc9
Андрей Васильев <skar67@yandex.ru>
2023-05-04 09:16:32 +0300

    Добавление источника данных для дашборда

--- Diploma/Program.cs
+++ Diploma/Program.cs
@@ -1,13 +1,18 @@
 using DevExpress.AspNetCore;
 using DevExpress.DashboardAspNetCore;
+using DevExpress.DashboardCommon;
 using DevExpress.DashboardWeb;
+using DevExpress.DataAccess.Json;
 using Diploma.Models;
 using Microsoft.Extensions.FileProviders;
+using System.Data.Common;
 
 var builder = WebApplication.CreateBuilder(args);
 
 IFileProvider? fileProvider = builder.Environment.ContentRootFileProvider;
 IConfiguration? configuration = builder.Configuration;
+
+IFileLoader loader = new DefaultFileLoader();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -19,6 +24,15 @@ builder.Services.AddScoped<DashboardConfigurator>((IServiceProvider serviceProvi
 {
     DashboardConfigurator configurator = new DashboardConfigurator();
     configurator.SetDashboardStorage(new DashboardFileStorage($"{Directory.GetCurrentDirectory()}\\AppData\\Dashboards"));
+    DataSourceInMemoryStorage dataSourceStorage = new DataSourceInMemoryStorage();
+
+
+
+    //DashboardJsonDataSource jsonDataSource = new DashboardJsonDataSource($"Json Data Source ");
+    //jsonDataSource.JsonSource = new CustomJsonSource(loader.GetFile("asdas.json"));
+    //dataSourceStorage.RegisterDataSource("objDataSource", objDataSource.SaveToXml());
+
+    configurator.SetDataSourceStorage(dataSourceStorage);
     configurator.SetConnectionStringsProvider(new DashboardConnectionStringsProvider(configuration));
     return configurator;
 });
@@ -32,6 +46,8 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+Console.WriteLine(loader.GetFile("asdas.json"));
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
